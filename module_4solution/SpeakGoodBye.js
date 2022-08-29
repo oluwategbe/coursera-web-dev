79a59dc0c1170a8a1f973844239a01ad8fb56d77
oluwategbe <pelumitegbe@gmail.com>
2022-08-29 11:59:12 +0100

    corrected again

--- module_4solution/SpeakGoodBye.js
+++ module_4solution/SpeakGoodBye.js
@@ -11,7 +11,7 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+// var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
@@ -21,10 +21,10 @@ var speakWord = "Good Bye";
 //   console.log(speakWord + " " + name);
 // })();
 
-(function(window){
+(function (window) {
     var byeSpeaker = {}
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
+    byeSpeaker.speak = function (name) {
+        console.log("Goodbye" + " " + name);
     }
     window.byeSpeaker = byeSpeaker;
 })(window);
--- module_4solution/SpeakHello.js
+++ module_4solution/SpeakHello.js
@@ -8,20 +8,20 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
+// var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
+ 
 // (function speak(name) {
 //   console.log(speakWord + " " + name);
 // })()
 
-(function(window){
+(function (window) {
     var helloSpeaker = {}
-    helloSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
+    helloSpeaker.speak = function (name) {
+        console.log("Hello" + " " + name);
     }
     window.helloSpeaker = helloSpeaker;
 })(window);
