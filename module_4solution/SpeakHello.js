1a6eeebc6bb71f1ad78cc19194a85108f597935f
oluwategbe <pelumitegbe@gmail.com>
2022-08-29 11:52:49 +0100

    corrected

--- module_4solution/SpeakHello.js
+++ module_4solution/SpeakHello.js
@@ -13,9 +13,10 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-(function speak(name) {
-  console.log(speakWord + " " + name);
-})()
+
+// (function speak(name) {
+//   console.log(speakWord + " " + name);
+// })()
 
 (function(window){
     var helloSpeaker = {}
